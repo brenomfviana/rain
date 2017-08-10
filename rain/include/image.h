faade8f38ed8656491d33770a43c19564e07e0f3
Breno Viana <brenomfviana@gmail.com>
2017-08-09 22:32:31 -0300

    Adds image and scene class.

---
+++ rain/include/image.h
@@ -0,0 +1,28 @@
+#ifndef _IMAGE_H_
+#define _IMAGE_H_
+
+class Image {
+
+    public:
+        // Image pixels
+        unsigned char* pixels;
+        // Image width
+        unsigned int width;
+        // Image height
+        unsigned int height;
+
+        /**
+         * Image constructor.
+         *
+         * @param width_ Image width
+         * @param height_ Image height
+         */
+        Image(int width_ = 0, int height_ = 0) : width(width_), height(height_) {
+            // Allocate pixel vector of image
+            pixels = new char(3 * width * height);
+        }
+};
+
+#include "image.cpp"
+
+#endif
