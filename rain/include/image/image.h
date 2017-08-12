65731f4909095618753c41b6ad5f3ed23f6f8f6d
Breno Viana <brenomfviana@gmail.com>
2017-08-12 19:43:43 -0300

    Updates image class.

--- rain/include/image/image.h
+++ rain/include/image/image.h
@@ -1,6 +1,9 @@
 #ifndef _IMAGE_H_
 #define _IMAGE_H_
 
+#include <string>
+#include "image_file_formats.h"
+
 /*!
  * This class represents an image.
  */
@@ -8,21 +11,48 @@ class Image {
 
     public:
         // Image pixels
-        unsigned char* pixels;
+        char* pixels;
         // Image width
         unsigned int width;
         // Image height
         unsigned int height;
+        // Image name
+        std::string name;
+        // File format
+        ImageFileFormats fileFormat;
+        // Image file codification
+        Codifications codification;
 
         /*!
          * Image constructor.
          *
          * @param width_ Image width
          * @param height_ Image height
+         * @param name Image name
+         * @param fileFormat_ Image file format
+         * @param codification_ Image file codification
          */
-        Image(int width_ = 0, int height_ = 0) : width(width_), height(height_) {
+        Image(int width_ = 0, int height_ = 0, std::string name_ = "unknown",
+            ImageFileFormats fileFormat_ = PPM, Codifications codification_ = BINARY) :
+            width(width_), height(height_), name(name_), fileFormat(fileFormat_),
+            codification(codification_) {
             // Allocate pixel vector of image
-            pixels = new char(3 * width * height);
+            pixels = new char[width * height * 3];
+            // Initializes pixels
+            for (unsigned int row = 0; row < height; row++) {
+    		   	for (unsigned int col = 0; col < width; col++) {
+                    pixels[(row * width * 3) + (col * 3)] = 0;
+                    pixels[(row * width * 3) + (col * 3) + 1] = 0;
+                    pixels[(row * width * 3) + (col * 3) + 2] = 0;
+                }
+            }
+        }
+
+        /*!
+         * Image destructor.
+         */
+        ~Image() {
+            delete [] pixels;
         }
 };
 
