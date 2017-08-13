b70b1a8a9bfc74f9aa88f713cc5bcc38654edd64
Breno Viana <brenomfviana@gmail.com>
2017-08-12 21:38:04 -0300

    Fixes rendering of images.

--- rain/src/render.cpp
+++ rain/src/render.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-#include <fstream>
 #include "utils/vec3.h"
 #include "render/ray.h"
 #include "image/image.h"
 #include "scene/scene_reader.h"
+#include "printer/printer.h"
 
 /**
  * Get the color of the reached pixel.
@@ -39,44 +39,37 @@ int main(int argc, char *argv[]) {
 		Image img(scene.width, scene.height, scene.name,
                   scene.type, scene.codification);
 		// Create camera
-		// Lower left corner of the view plane.
-		Point3 lower_left_corner(-2, -1, -1);
-		// Horizontal dimension of the view plane.
+		// Lower left corner of the view plane
+		Point3 llc(-2, -1, -1);
+		// Horizontal dimension of the view plane
 	    Vec3 horizontal(4, 0, 0);
-		// Vertical dimension of the view plane.
+		// Vertical dimension of the view plane
 	    Vec3 vertical(0, 2, 0);
-		// The camera's origin.
+		// The camera's origin
 	    Point3 origin(0, 0, 0);
 
 		// Print image
-	   	for (int row = (scene.height - 1); row >= 0; --row) {     // Y
-		   	for(unsigned int col = 0; col < scene.width; col++) { // X
+	   	for (unsigned int row = 0, i = (img.height - 1); row < img.height; row++, i--) {   // Y
+            // Walked v% of the vertical dimension of the view plane
+            float v = float(row) / float(img.height);
+		   	for(unsigned int col = 0; col < img.width; col++) { // X
 				// Walked u% of the horizontal dimension of the view plane
 				float u = float(col) / float(img.width);
-				// Walked v% of the vertical dimension of the view plane
-				float v = float(row) / float(img.height);
-				Point3 end_point = lower_left_corner + (u * horizontal) + (v * vertical);
+				Point3 endPoint = llc + (u * horizontal) + (v * vertical);
 				// The ray
-				Ray r(origin, end_point - origin);
+				Ray r(origin, endPoint - origin);
 				auto c = color(r, scene);
 				// Print pixel
 				int ir = int(255.99f * c[RGB::R]);
 				int ig = int(255.99f * c[RGB::G]);
 				int ib = int(255.99f * c[RGB::B]);
-                img.pixels[(row * img.width * 3) + (col * 3)] = ir;
-                img.pixels[(row * img.width * 3) + (col * 3) + 1] = ig;
-                img.pixels[(row * img.width * 3) + (col * 3) + 2] = ib;
+                img.pixels[(i * img.width * 3) + (col * 3)] = ir;
+                img.pixels[(i * img.width * 3) + (col * 3) + 1] = ig;
+                img.pixels[(i * img.width * 3) + (col * 3) + 2] = ib;
 			}
 		}
-
-        std::ofstream outfile(scene.name, std::ofstream::binary);
-
-    	outfile << "P6" << std::endl;
-    	outfile << img.width << " " << img.height << std::endl;
-    	outfile << "255" << std::endl;
-
-    	outfile.write(img.pixels, img.width * img.height * 3);
-      	outfile.close();
+        // Print image
+        Printer::print(img, "");
 	}
     return 0;
 }
