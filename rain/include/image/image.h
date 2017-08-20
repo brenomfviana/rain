9fd899f778633b0dcdb87e382bf2da6e8314d8b3
Breno Viana <brenomfviana@gmail.com>
2017-08-20 16:58:12 -0300

    Removes unnecessary attributes.

--- rain/include/image/image.h
+++ rain/include/image/image.h
@@ -1,9 +1,6 @@
 #ifndef _IMAGE_H_
 #define _IMAGE_H_
 
-#include <string>
-#include "printer/image_file_formats.h"
-
 /*!
  * This class represents an image.
  */
@@ -16,26 +13,15 @@ class Image {
         unsigned int width;
         // Image height
         unsigned int height;
-        // Image name
-        std::string name;
-        // File format
-        ImageFileFormats fileFormat;
-        // Image file codification
-        Codifications codification;
 
         /*!
          * Image constructor.
          *
          * @param width_ Image width
          * @param height_ Image height
-         * @param name_ Image name
-         * @param fileFormat_ Image file format
-         * @param codification_ Image file codification
          */
-        Image(int width_ = 0, int height_ = 0, std::string name_ = "unknown",
-            ImageFileFormats fileFormat_ = PPM, Codifications codification_ = BINARY) :
-            width(width_), height(height_), name(name_), fileFormat(fileFormat_),
-            codification(codification_) {
+        Image(unsigned int width_ = 0, unsigned int height_ = 0) :
+            width(width_), height(height_) {
             // Allocate pixel vector of image
             pixels = new char[width * height * 3];
             // Initialize pixels
