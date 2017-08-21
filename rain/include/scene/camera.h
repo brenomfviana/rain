ec8cb306e70a7fae55a14009555036ea858e6807
Breno Viana <brenomfviana@gmail.com>
2017-08-21 14:17:16 -0300

    Adds camera class.

--- rain/include/scene/camera.h
+++ rain/include/scene/camera.h
@@ -18,7 +18,8 @@ class Camera {
         /*!
          * Camera constructor.
          */
-        Camera(Point3 llc_, Vec3 horizontal_, Vec3 vertical_, Point3 origin_) :
+        Camera(Point3 llc_ = Point3(-8, -4.5, -4.5), Vec3 horizontal_ = Vec3(16, 0, 0),
+            Vec3 vertical_ = Vec3(0, 9, 0), Point3 origin_ = Point3(0, 0, 0)) :
             llc(llc_), horizontal(horizontal_), vertical(vertical_), origin(origin_)
             { /* empty */ }
 };
