3738e4d9bd5287ed8a71754d867d2ed3a685ee80
Breno Viana <brenomfviana@gmail.com>
2017-09-09 23:54:47 -0300

    Implements exception handling.

--- rain/src/rain.cpp
+++ rain/src/rain.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "image/image.h"
 #include "scene/scene.h"
 #include "scene/camera.h"
@@ -12,22 +13,23 @@ int main(int argc, char *argv[]) {
 	// Check for a valid number of arguments
 	if (argc != 2) {
 		// ERROR
-		std::cout << "Error: Invalid number of arguments!" << std::endl;
+		std::cout << "\033[1;31mError: Invalid number of arguments!\033[0m\n";
 	} else {
-		// TODO: Check if the file exists
-        Scene scene;
-		OutputSettings os;
-		Shader* shader;
-        Camera cam;
-		// Read file
-        SceneReader::read(argv[1], scene, cam, shader, os);
-		// TODO: Check shader
-
-        // Render
-        Image img = *(RayTracer::render(cam, scene, shader, os.width, os.height,
-			 							10));
-        // Print image
-        Printer::print(img, os, "");
+		try {
+	        Scene scene;
+			OutputSettings os;
+			Shader* shader;
+	        Camera cam;
+			// Read file
+        	SceneReader::read(argv[1], scene, cam, shader, os);
+			// Render
+	        Image img = *(RayTracer::render(cam, scene, shader, os.width,
+				 							os.height, 10));
+	        // Print image
+	        Printer::print(img, os, "");
+		} catch (const char* e) {
+			std::cout << "\033[1;31m" << e << "\033[0m\n";
+		}
 	}
     return 0;
 }
